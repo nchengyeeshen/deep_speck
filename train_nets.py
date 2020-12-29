0b6f05bbfb6c2456572ff889e16f4e1b031eb2f5
Nicholas Cheng <nchengyeeshen@gmail.com>
2020-12-29 22:18:53 +0800

    Prettify make_resnet

--- train_nets.py
+++ train_nets.py
@@ -37,7 +37,6 @@ def make_checkpoint(filepath: str) -> ModelCheckpoint:
     return ModelCheckpoint(filepath, monitor="val_loss", save_best_only=True)
 
 
-# make residual tower of convolutional blocks
 def make_resnet(
     num_blocks=2,
     num_filters=32,
@@ -50,18 +49,21 @@ def make_resnet(
     reg_param=0.0001,
     final_activation="sigmoid",
 ):
+    """Make residual tower of convolutional blocks."""
     # Input and preprocessing layers
     inp = Input(shape=(num_blocks * word_size * 2,))
     rs = Reshape((2 * num_blocks, word_size))(inp)
     perm = Permute((2, 1))(rs)
-    # add a single residual layer that will expand the data to num_filters channels
-    # this is a bit-sliced layer
+
+    # Add a single residual layer that will expand the data to num_filters
+    # channels This is a bit-sliced layer.
     conv0 = Conv1D(
         num_filters, kernel_size=1, padding="same", kernel_regularizer=l2(reg_param)
     )(perm)
     conv0 = BatchNormalization()(conv0)
     conv0 = Activation("relu")(conv0)
-    # add residual blocks
+
+    # Add residual blocks
     shortcut = conv0
     for _ in range(depth):
         conv1 = Conv1D(
@@ -72,6 +74,7 @@ def make_resnet(
         )(shortcut)
         conv1 = BatchNormalization()(conv1)
         conv1 = Activation("relu")(conv1)
+
         conv2 = Conv1D(
             num_filters,
             kernel_size=ks,
@@ -80,19 +83,26 @@ def make_resnet(
         )(conv1)
         conv2 = BatchNormalization()(conv2)
         conv2 = Activation("relu")(conv2)
+
         shortcut = Add()([shortcut, conv2])
-    # add prediction head
+
+    # Add prediction head
     flat1 = Flatten()(shortcut)
+
     dense1 = Dense(d1, kernel_regularizer=l2(reg_param))(flat1)
     dense1 = BatchNormalization()(dense1)
     dense1 = Activation("relu")(dense1)
+
     dense2 = Dense(d2, kernel_regularizer=l2(reg_param))(dense1)
     dense2 = BatchNormalization()(dense2)
     dense2 = Activation("relu")(dense2)
+
     out = Dense(
         num_outputs, activation=final_activation, kernel_regularizer=l2(reg_param)
     )(dense2)
+
     model = Model(inputs=inp, outputs=out)
+
     return model
 
 
